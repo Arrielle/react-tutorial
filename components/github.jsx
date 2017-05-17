cd69c8ca1c878e617daab69092260613c1189ce2
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-05-16 22:50:31 -0500

    dynamic username for github cards is working

--- components/github.jsx
+++ components/github.jsx
@@ -1,19 +1,20 @@
 //box that displays the avatar and the name of the user
 var Card = React.createClass({
   //initialize the component with an empty state object
-  getInialState: function(){
+  getInitialState: function(){
     return{};
   },
-  //find the right time to make the component fetch the date
+  //determine the right time to make the component fetch the data
   //lifecycle hook - right after the component is mounted in the DOM
   componentDidMount: function(){
-    console.log('I was triggered during componentDidMount')
+    console.log('I was triggered during componentDidMount');
     var component = this;
     $.get("https://api.github.com/users/" + this.props.login, function(data){
       component.setState(data);
       console.log('what is the data?', data);
     });
   },
+  //Render the html
   render: function(){
     return (
       <div>
@@ -29,7 +30,7 @@ var Main = React.createClass({
   render: function(){
     return (
       <div>
-        <Card username="Arrielle"/>
+        <Card login="Arrielle"/>
       </div>
     )
   }
