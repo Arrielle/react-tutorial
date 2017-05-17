b56bb9cc888b7183f3e02a2f3b115c1544a08272
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-05-16 21:52:55 -0500

    Initial commit

---
+++ script.jsx
@@ -0,0 +1,15 @@
+var Button = React.createClass({
+
+  getInitialState: function(){
+    return {counter:0};
+  },
+
+  render: function(){
+    return (
+      <button>{this.state.counter}</button>
+    )
+  }
+  
+});
+
+React.render(<Button />, document.getElementById("root"));
